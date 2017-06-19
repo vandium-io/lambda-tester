f32e7a91ebbb7526e9be98921f14c884c0b53b65
rhyatt <richard@vandium.io>
2017-06-19 16:02:13 -0400

    added

---
+++ test/lib/xray/trace_id.test.js
@@ -0,0 +1,35 @@
+'use strict';
+
+/*jshint expr: true*/
+
+const expect = require( 'chai' ).expect;
+
+const createTraceId = require( '../../../lib/xray/trace_id' );
+
+describe( 'lib/xray/trace_id', function() {
+
+    it( 'normal operation', function() {
+
+        let traceId = createTraceId();
+
+        let verifyRegEx = /(\w+)[=]([\w\-]+)\;?/g
+
+        let rootMatch = verifyRegEx.exec( traceId );
+        expect( rootMatch ).to.exist;
+        expect( rootMatch[1] ).to.equal( 'Root' );
+        expect( rootMatch[2].startsWith( '1-' ) ).to.be.true;
+        expect( rootMatch[2].length ).to.equal( 35 );
+
+        let parentMatch = verifyRegEx.exec( traceId );
+        expect( parentMatch ).to.exist;
+        expect( parentMatch[1] ).to.equal( 'Parent' );
+        expect( parentMatch[2].length ).to.equal( 16 );
+
+        let sampledMatch = verifyRegEx.exec( traceId );
+        expect( sampledMatch ).to.exist;
+        expect( sampledMatch[1] ).to.equal( 'Sampled' );
+        expect( sampledMatch[2] ).to.equal( '1' );
+
+        expect( verifyRegEx.exec( traceId ) ).to.be.null;
+    });
+});
